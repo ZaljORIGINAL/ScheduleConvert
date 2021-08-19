be204c5af6a7ddac54bbbb4df3c5c959d8774dc8
nuriakhmetov_am <almaz.zalj@mail.ru>
2021-08-19 08:59:42 +0300

    Создан проект

---
+++ src/main/java/ExcelReader.java
@@ -0,0 +1,3 @@
+public class ExcelReader {
+    /*https://www.viralpatel.net/java-read-write-excel-file-apache-poi/*/
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException {
+        Path schedule = Paths.get(
+                Main.class.getResource("Osen_IS_2020-2021.xls")
+                        .toURI());
+
+
+    }
+}
